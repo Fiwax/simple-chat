a7f29c83f16a4ac8ddeb304bfaae565e3308a00f
Fiwax <goldeneagle9001@gmail.com>
2021-04-22 17:51:17 +0300

    some fixes

--- client/components/adminPage/adminSidebar.js
+++ client/components/adminPage/adminSidebar.js
@@ -11,7 +11,7 @@ const AdminSidebar = () => {
 
   return (
     <div className="admin-sidebar min-w-max inset-y-0 left-0 transform -translate-x-full md:translate-x-0 absolute md:relative transition duration-200 ease-in-out w-1/5 h-screen pb-6 overflow-y-auto bg-gray-800 md:block">
-      <h1 className="flex justify-between px-4 mt-3 mb-2 font-sans text-xl text-white">
+      <h1 className="flex justify-start px-4 mt-3 mb-2 font-sans text-xl text-white">
         <span>Tailwind CSS</span>
       </h1>
       <div className="flex items-center justify-between px-4 py-1 mb-2">
@@ -19,28 +19,6 @@ const AdminSidebar = () => {
           <span className="w-2 h-2 my-2 bg-green-500 rounded-full" />
           <span className="text-white ">{full_name}</span>
         </div>
-        <span className="flex w-5 h-5 text-white cursor-pointer ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-        </span>
       </div>
       <div>
         <button
