44219d2850f4b0d18ebade9f708b7e4926c01fb3
Fiwax <goldeneagle9001@gmail.com>
2021-04-22 17:44:01 +0300

    added the size of svg element

--- client/components/sideBar.js
+++ client/components/sideBar.js
@@ -52,6 +52,7 @@ const SideBar = () => {
             <span className="flex w-5 h-5 text-white cursor-pointer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
